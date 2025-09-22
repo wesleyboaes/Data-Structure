315d95b5f28e07810452780d21facdd6159c921d
Wesley Boaes <wesleyboaes@gmail.com>
2025-09-22 10:44:49 -0300

    Lists - manual mode

--- lists_manual.c
+++ lists_manual.c
@@ -22,11 +22,16 @@ int main()
     p1 -> x = 3;
     p1 -> y = 9;
 
-    p -> next = p1; // I use as reference the pointer "p" to point to the next one, that is "p1", using the pointer "next" that have the same type "point" as "p".
-    p1 -> next = NULL; // Here the "p1" does not have any pointer to point yet, so it is "NULL".
+    Point *p2 = (Point*) malloc(sizeof(Point));
 
-    printf("Hello World");
-    printf("Hello World");
-    
+    p2 -> x = 8;
+    p2 -> y = 13;
+
+    /* This is the manual form to chain elements. */
+    p -> next = p1; // I use as reference the pointer "p" to point to the next one, that is "p1", using the pointer "next" that have the same type "point" as "p".
+    // p1 -> next = NULL; // Here the "p1" does not have any pointer to point yet, so it is "NULL".
+    p1 -> next = p2; // Here the "p1" point to the next one, "p2".
+    p2 -> next = NULL; // The same as the "p1" example (when "p2" did not exists).
+
     return 0;
 }
